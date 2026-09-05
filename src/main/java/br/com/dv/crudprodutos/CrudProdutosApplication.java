27a31845cfb364af89e6300baebdac5a7df405c4
GabrielDOSilva <gabriel.silva781.09@gmail.com>
2026-09-05 14:57:20 -0300

    Chore: create project package structure

--- src/main/java/br/com/dv/crudprodutos/CrudProdutosApplication.java
+++ src/main/java/br/com/dv/crudprodutos/CrudProdutosApplication.java
@@ -1,4 +1,4 @@
-package br.com.dv.crud_produtos;
+package br.com.dv.crudprodutos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/dv/crudprodutos/CrudProdutosApplicationTests.java
+++ src/test/java/br/com/dv/crudprodutos/CrudProdutosApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.dv.crud_produtos;
+package br.com.dv.crudprodutos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
