adea0525165ac66716cbbc6275af9972f824eb31
GabrielDOSilva <gabriel.silva781.09@gmail.com>
2026-09-06 13:43:52 -0300

    feat: standardize product error response

---
+++ src/main/java/br/com/dev/crudprodutos/dto/ErrorResponseDTO.java
@@ -0,0 +1,34 @@
+package br.com.dev.crudprodutos.dto;
+
+import java.time.LocalDateTime;
+
+public class ErrorResponseDTO {
+	
+	private int status;
+	private String message;
+	private LocalDateTime timestamp;
+	
+	public ErrorResponseDTO(int status, String message, LocalDateTime timestamp) {
+		
+		this.status = status;
+		this.message = message;
+		this.timestamp = timestamp;
+	}
+
+
+	public int getStatus() {
+		return status;
+	}
+
+
+	public String getMessage() {
+		return message;
+	}
+
+
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+
+
+}
--- src/main/java/br/com/dev/crudprodutos/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/dev/crudprodutos/exception/GlobalExceptionHandler.java
@@ -1,19 +1,27 @@
 package br.com.dev.crudprodutos.exception;
 
+import java.time.LocalDateTime;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import br.com.dev.crudprodutos.dto.ErrorResponseDTO;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(ProductNotFoundException.class)
-	public ResponseEntity<String> handleProductNotFound(ProductNotFoundException exception){
+	public ResponseEntity<ErrorResponseDTO> handleProductNotFound(
+			ProductNotFoundException exception){
+		ErrorResponseDTO error = new ErrorResponseDTO(
+				HttpStatus.NOT_FOUND.value(), 
+				exception.getMessage(), 
+				LocalDateTime.now()
+				);
 		
-		return ResponseEntity
-				.status(HttpStatus.NOT_FOUND)
-				.body(exception.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 		
 	}
 
