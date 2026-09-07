b95dce1b9723f799137ad353d9ec39b055e783d8
GabrielDOSilva <gabriel.silva781.09@gmail.com>
2026-09-07 10:24:16 -0300

    feat: return created status por product creation

--- src/main/java/br/com/dev/crudprodutos/controller/ProductController.java
+++ src/main/java/br/com/dev/crudprodutos/controller/ProductController.java
@@ -2,6 +2,7 @@ package br.com.dev.crudprodutos.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,7 +35,9 @@ public class ProductController {
 
         ProductResponseDTO response = productService.create(productRequestDTO);
 
-        return ResponseEntity.ok(response);
+        return ResponseEntity
+        		.status(HttpStatus.CREATED)
+        		.body(response);
     }
     
     @GetMapping
